fb3738694547131835ae5c521a7bf0afe243d69b
jcamou <jccamou@gmail.com>
2017-09-26 19:59:55 -0400

    [ADD] add gulp support and live reload

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const child        = require('child_process');
+const browserSync  = require('browser-sync').create();
+
+const gulp         = require('gulp');
+const concat       = require('gulp-concat');
+const sass         = require('gulp-sass');
+const cssnano      = require('gulp-cssnano');
+const autoprefix   = require('gulp-autoprefixer');
+const gutil        = require('gulp-util');
+
+const siteRoot     = '_site';
+const cssFiles     = '_app/_css/**/master.scss';
+
+gulp.task('test', function() {
+  console.log('Hello, Test');
+});
+
+
+gulp.task('css', () => {
+  gulp.src(cssFiles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('_app/assets'));
+});
+
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['build',
+    '--watch',
+    '--incremental',
+    '--drafts'
+  ]);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+
+});
+
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
+
+  gulp.watch(cssFiles, ['css']);
+});
+
+
+gulp.task('default', ['css', 'jekyll', 'serve']);
